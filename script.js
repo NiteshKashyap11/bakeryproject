69e3f928cc4d76a624b7b568a45a6e4b1a182e74
NiteshKashyap11 <151061470+NiteshKashyap11@users.noreply.github.com>
2023-11-16 22:24:41 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+let currentSlide = 0;
+
+function showSlide(index) {
+    const slides = document.getElementsByClassName('carousel-slide');
+    
+    if (index >= slides.length) {
+        currentSlide = 0;
+    } else if (index < 0) {
+        currentSlide = slides.length - 1;
+    } else {
+        currentSlide = index;
+    }
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = 'none';
+    }
+
+    slides[currentSlide].style.display = 'block';
+}
+
+function nextSlide() {
+    showSlide(currentSlide + 1);
+}
+
+function prevSlide() {
+    showSlide(currentSlide - 1);
+}
+
+setInterval(nextSlide, 3000);
